cf5a197481532082ac277e532881bde903bdd9fc
Pascal Wichmann <wichmannpas@gmail.com>
2016-08-08 21:51:04 +0200

    Added slight modification of niklas's watch script

---
+++ stine_exam_watcher.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+i="0"
+i=$(./stine.py getexams | wc -l)
+while true
+do
+  new=$(./stine.py getexams | wc -l)
+  
+  if [ "$i" == "$new" ];
+  then
+    echo nothing happened
+  else
+    echo AAAAHHH!!! Panik!
+  fi
+  i=new
+
+  sleep 2m
+done
