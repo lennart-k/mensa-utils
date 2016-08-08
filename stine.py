fb475b5de154a0668e531281e1e4692b0e1dbe29
Pascal Wichmann <wichmannpas@gmail.com>
2016-08-08 14:27:45 +0200

    Updated parser for stine session file

--- stine.py
+++ stine.py
@@ -3,16 +3,24 @@
 from argparse import ArgumentParser
 
 
+def start_session(arguments):
+    """Start a new session."""
+    pass
+
+
 if __name__ == '__main__':
     parser = ArgumentParser(description='Pseudo-API for stine.')
-    subparsers = parser.add_subparsers()
+    subparsers = parser.add_subparsers(dest='subcommand')
+    subparsers.required = True
 
     parser_startsession = subparsers.add_parser(
         'startsession', help='Authenticate for a new session.')
+    parser_startsession.add_argument(
+        '-s', '--session-file', nargs='?', default='./stine_session',
+        help='The file to store the session data in.')
 
     parser_getexams = subparsers.add_parser(
         'getexams', help='Get current exam results.')
-    parser_getexams.add_argument('foo', help='Foo Bar')
 
     arguments = parser.parse_args()
-    print(arguments.action)
+    print(arguments)
