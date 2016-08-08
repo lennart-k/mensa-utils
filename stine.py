d0002aaf3a39278d78a28525ee0562f7bbde1da0
Pascal Wichmann <wichmannpas@gmail.com>
2016-08-08 19:03:16 +0200

    Basic session initiation working

--- stine.py
+++ stine.py
@@ -1,11 +1,57 @@
 #!/usr/bin/env python3
 """Some stine pseudo-api tools."""
 from argparse import ArgumentParser
+from getpass import getpass
+import pickle
+import re
+import requests
+
+
+STINE_BASE_URL = 'https://www.stine.uni-hamburg.de/'
+STINE_ENCODING = 'UTF-16LE'
 
 
 def start_session(arguments):
     """Start a new session."""
-    pass
+    username = input('Username: ')
+    password = getpass()
+    session = requests.Session()
+
+    # get stine home page (for required cookies)
+    home_page = session.get(STINE_BASE_URL)
+    home_page.encoding = STINE_ENCODING
+
+    # search for main link for german language
+    pattern = re.compile(r'href="([^"]*)">de<')
+    match = pattern.search(home_page.text)
+    if not match:
+        print('Invalid stine home page.')
+        return
+    a = session.get(STINE_BASE_URL + match.group(1))
+    # return
+
+    # try to authenticate
+    login_result = session.post(
+        STINE_BASE_URL + 'scripts/mgrqispi.dll', data={
+            'usrname': username,
+            'pass': password,
+            'APPNAME': 'CampusNet',
+            'PRGNAME': 'LOGINCHECK',
+            'ARGUMENTS': 'clino,usrname,pass,menuno,menu_type,browser,platform',
+            'clino': '000000000000001',
+            'menuno': '000000',
+            'menu_type': 'classic',
+            'browser': '',
+            'platform': '',
+        })
+    error_pattern = re.compile(r'Kennung oder Kennwort falsch')
+    if error_pattern.search(login_result.text):
+        print('Access denied.')
+        return
+
+    with open(arguments.session_file, 'wb') as session_file:
+        pickle.dump(session, session_file)
+    print('Session successfully initiated.')
 
 
 if __name__ == '__main__':
@@ -17,10 +63,11 @@ if __name__ == '__main__':
         'startsession', help='Authenticate for a new session.')
     parser_startsession.add_argument(
         '-s', '--session-file', nargs='?', default='./stine_session',
-        help='The file to store the session data in.')
+        dest='session_file', help='The file to store the session data in.')
 
     parser_getexams = subparsers.add_parser(
         'getexams', help='Get current exam results.')
 
     arguments = parser.parse_args()
-    print(arguments)
+    if arguments.subcommand == 'startsession':
+        start_session(arguments)
