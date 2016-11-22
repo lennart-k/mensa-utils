9dac2634737a22be8a51722adac97a22c039869d
Pascal Wichmann <wichmannpas@gmail.com>
2016-11-22 21:11:43 +0100

    Basic models

--- mensautils/base/models.py
+++ mensautils/base/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Dish(models.Model):
+    name = models.CharField(verbose_name=_('name'), max_length=300)
+
+
+class Canteen(models.Model):
+    name = models.CharField(verbose_name=_('name'), max_length=100)
+
+
+class Serving(models.Model):
+    date = models.DateField()
+    dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
+    canteen = models.ForeignKey(Canteen, on_delete=models.CASCADE)
