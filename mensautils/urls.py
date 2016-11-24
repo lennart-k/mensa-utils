5372163d26dfef6f319e0208eb782881dca005e8
Pascal Wichmann <wichmannpas@gmail.com>
2016-11-24 21:20:46 +0100

    Added debug toolbar when DEBUG is True

--- mensautils/urls.py
+++ mensautils/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
@@ -25,3 +26,9 @@ urlpatterns = [
     url('^logout/$', auth_views.logout, name='logout'),
     url(r'^', include('mensautils.canteen.urls'))
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
