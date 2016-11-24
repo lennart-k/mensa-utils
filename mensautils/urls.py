480ea71966bf40b28767dd35ebec2efcad9db4ca
Pascal Wichmann <wichmannpas@gmail.com>
2016-11-24 19:20:32 +0100

    Basic authentication

--- mensautils/urls.py
+++ mensautils/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url('^login/$', auth_views.login, {
+        'template_name': 'mensautils/login.html',
+    }, name='login'),
+    url('^logout/$', auth_views.logout, name='logout'),
     url(r'^', include('mensautils.canteen.urls'))
 ]
