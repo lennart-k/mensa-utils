e740fee6e40dcce57bb613db5de806f20d140c7d
Pascal Wichmann <wichmannpas@gmail.com>
2016-11-28 08:37:12 +0100

    Update last_notified only of notifications yielding a mail

--- mensautils/canteen/notifications.py
+++ mensautils/canteen/notifications.py
@@ -12,19 +12,23 @@ def send_notifications():
     now = timezone.now()
     today = date.today()
     relevant_servings = Serving.objects.filter(date=today, notified=False)
-    notifications = Notification.objects.all()
-    for serving in relevant_servings.all():
-        for notification in notifications:
+    notification_objs = Notification.objects.all()
+    notified = set()
+    for notification in notification_objs:
+        for serving in relevant_servings.all():
             if (fuzz.partial_token_set_ratio(serving.dish.name, notification.pattern) >=
                settings.FUZZY_MIN_RATIO):
                 subject = 'Mensabenachrichtigung: {} in {}'.format(
                     serving.dish.name, serving.canteen.name)
-                message = '''Das Gericht {} wird heute in der Mensa {} angeboten.
+                message = '''Das Gericht {} wird heute in der Mensa {} zu einem Preis von {} € angeboten.
 
-                Du erhältst diese Nachricht, weil Du unter https://mensa.mafiasi.de eine
-                Benchrichtigung mit der Suchregel "{}" angelegt hast.'''.format(
-                    serving.dish.name, serving.canteen.name, notification.pattern)
+Du erhältst diese Nachricht, weil Du unter https://mensa.mafiasi.de eine
+Benchrichtigung mit der Suchregel "{}" angelegt hast.'''.format(
+                    serving.dish.name, serving.canteen.name, serving.price,
+                    notification.pattern)
                 send_mail(subject, message, settings.NOTIFICATION_FROM_ADDRESS,
                           [notification.user.email], fail_silently=False)
-    notifications.update(last_notified=now)
+                notified.add(notification.pk)
+                break  # skip more notifications for this user notification
+    notification_objs.filter(pk__in=notified).update(last_notified=now)
     relevant_servings.update(notified=True)
